84c9934e06b5b258e00b6069cba06e397440bc59
Boris van Breugel <boris-van-breugel@live.nl>
2023-03-17 07:34:34 +0000

    CIFAR conditional generation

--- DiffusionFreeGuidence/TrainCondition.py
+++ DiffusionFreeGuidence/TrainCondition.py
@@ -9,8 +9,9 @@ import torch.optim as optim
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from torchvision.datasets import CIFAR10
+from torchvision.datasets import CIFAR10, CIFAR100
 from torchvision.utils import save_image
+import os
 
 from DiffusionFreeGuidence.DiffusionCondition import GaussianDiffusionSampler, GaussianDiffusionTrainer
 from DiffusionFreeGuidence.ModelCondition import UNet
@@ -20,17 +21,32 @@ from Scheduler import GradualWarmupScheduler
 def train(modelConfig: Dict):
     device = torch.device(modelConfig["device"])
     # dataset
-    dataset = CIFAR10(
-        root='./CIFAR10', train=True, download=True,
-        transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-        ]))
+    if modelConfig["dataset"] == "CIFAR10":
+
+        dataset = CIFAR10(
+            root='./CIFAR10', train=True, download=True,
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]))
+        num_labels = 10
+    elif modelConfig["dataset"] == "CIFAR100":
+        dataset = CIFAR100(
+            root='./CIFAR100', train=True, download=True,
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]))
+        num_labels = 100
+    else:
+        raise NotImplementedError
+    
     dataloader = DataLoader(
         dataset, batch_size=modelConfig["batch_size"], shuffle=True, num_workers=4, drop_last=True, pin_memory=True)
 
     # model setup
-    net_model = UNet(T=modelConfig["T"], num_labels=10, ch=modelConfig["channel"], ch_mult=modelConfig["channel_mult"],
+    
+    net_model = UNet(T=modelConfig["T"], num_labels=num_labels, ch=modelConfig["channel"], ch_mult=modelConfig["channel_mult"],
                      num_res_blocks=modelConfig["num_res_blocks"], dropout=modelConfig["dropout"]).to(device)
     if modelConfig["training_load_weight"] is not None:
         net_model.load_state_dict(torch.load(os.path.join(
@@ -68,8 +84,9 @@ def train(modelConfig: Dict):
                     "LR": optimizer.state_dict()['param_groups'][0]["lr"]
                 })
         warmUpScheduler.step()
-        torch.save(net_model.state_dict(), os.path.join(
-            modelConfig["save_dir"], 'ckpt_' + str(e) + "_.pt"))
+        if (e % 20 == 0 and e>20) or e == modelConfig["epoch"] - 1:
+            torch.save(net_model.state_dict(), os.path.join(
+                modelConfig["save_dir"], 'ckpt_' + str(e) + "_.pt"))
 
 
 def eval(modelConfig: Dict):
@@ -99,10 +116,51 @@ def eval(modelConfig: Dict):
         noisyImage = torch.randn(
             size=[modelConfig["batch_size"], 3, modelConfig["img_size"], modelConfig["img_size"]], device=device)
         saveNoisy = torch.clamp(noisyImage * 0.5 + 0.5, 0, 1)
-        save_image(saveNoisy, os.path.join(
-            modelConfig["sampled_dir"],  modelConfig["sampledNoisyImgName"]), nrow=modelConfig["nrow"])
+        # save_image(saveNoisy, os.path.join(
+        #     modelConfig["sampled_dir"],  modelConfig["sampledNoisyImgName"]), nrow=modelConfig["nrow"])
         sampledImgs = sampler(noisyImage, labels)
         sampledImgs = sampledImgs * 0.5 + 0.5  # [0 ~ 1]
         print(sampledImgs)
         save_image(sampledImgs, os.path.join(
             modelConfig["sampled_dir"],  modelConfig["sampledImgName"]), nrow=modelConfig["nrow"])
+        
+def generate(modelConfig: Dict):
+    samples_per_class = modelConfig["samples_per_class"] 
+    p_train = modelConfig["p_train"]
+    device = torch.device(modelConfig["device"])
+    # load model and evaluate
+    with torch.no_grad():
+        model = UNet(T=modelConfig["T"], num_labels=10, ch=modelConfig["channel"], ch_mult=modelConfig["channel_mult"],
+                     num_res_blocks=modelConfig["num_res_blocks"], dropout=modelConfig["dropout"]).to(device)
+        ckpt = torch.load(os.path.join(
+            modelConfig["save_dir"], modelConfig["test_load_weight"]), map_location=device)
+        model.load_state_dict(ckpt)
+        print("model load weight done.")
+        model.eval()
+        sampler = GaussianDiffusionSampler(
+            model, modelConfig["beta_1"], modelConfig["beta_T"], modelConfig["T"], w=modelConfig["w"]).to(device)
+        for label in range(modelConfig['num_classes']):
+            num_batches = samples_per_class // modelConfig["batch_size_generation"]
+            start_index = 0
+            for batch in range(num_batches):
+                batch_size = min([modelConfig["batch_size_generation"], samples_per_class - start_index])
+                labels = (torch.ones(size=[batch_size]).long() * label+1).to(device)
+                # Sampled from standard normal distribution
+                noisyImage = torch.randn(
+                    size=[batch_size, 3, modelConfig["img_size"], modelConfig["img_size"]], device=device)
+                sampledImgs = sampler(noisyImage, labels)
+                sampledImgs = sampledImgs * 0.5 + 0.5  # [0 ~ 1]
+                for j in range(batch_size):
+                    if j +start_index < samples_per_class * p_train:
+                        path = os.path.join(
+                            modelConfig["sampled_dir"], 'train', str(label))
+                        os.makedirs(path, exist_ok=True)
+                        save_image(sampledImgs[j], os.path.join(path, str(j+start_index) + ".png"))
+                    else:
+                        path = os.path.join(
+                            modelConfig["sampled_dir"], 'test', str(label))
+                        os.makedirs(path, exist_ok=True)
+                        save_image(sampledImgs[j], os.path.join(
+                           path, str(j+start_index) + ".png"))
+                        
+                start_index += batch_size
--- MainCondition.py
+++ MainCondition.py
@@ -1,17 +1,18 @@
-from DiffusionFreeGuidence.TrainCondition import train, eval
+from DiffusionFreeGuidence.TrainCondition import train, eval, generate
+import os
 
-
-def main(model_config=None):
+def main(model_config=None, **kwargs):
     modelConfig = {
-        "state": "train", # or eval
-        "epoch": 70,
+        "state": "traingen", # or eval
+        "epoch": 40,
         "batch_size": 80,
-        "T": 500,
+        "batch_size_generation": 512,
+        "T": 100,
         "channel": 128,
         "channel_mult": [1, 2, 2, 2],
         "num_res_blocks": 2,
         "dropout": 0.15,
-        "lr": 1e-4,
+        "lr": 2e-4,
         "multiplier": 2.5,
         "beta_1": 1e-4,
         "beta_T": 0.028,
@@ -19,20 +20,57 @@ def main(model_config=None):
         "grad_clip": 1.,
         "device": "cuda:0",
         "w": 1.8,
-        "save_dir": "./CheckpointsCondition/",
+        "save_dir": "./CheckpointsConditionV2",
         "training_load_weight": None,
-        "test_load_weight": "ckpt_63_.pt",
-        "sampled_dir": "./SampledImgs/",
+        "sampled_dir": "./SampledImgsV2",
         "sampledNoisyImgName": "NoisyGuidenceImgs.png",
         "sampledImgName": "SampledGuidenceImgs.png",
-        "nrow": 8
+        "nrow": 8,
+        "seed": 'range', 
+        "dataset": "CIFAR10",
+        "samples_per_class": 6000,
+        "p_train": 5/6,
+
     }
+
+    
     if model_config is not None:
         modelConfig = model_config
-    if modelConfig["state"] == "train":
-        train(modelConfig)
+        modelConfig["test_load_weight"] = f"ckpt_{modelConfig['epoch']-1}_.pt"
+        
+    
+    if kwargs is not None:
+        for key, value in kwargs.items():
+            modelConfig[key] = value
+    
+    if modelConfig['seed'] == 'range':
+        seeds = range(5)
     else:
-        eval(modelConfig)
+        seeds = [modelConfig['seed']]
+
+    for seed in seeds:
+        modelConfig['seed'] = seed
+        if modelConfig['dataset'] == 'CIFAR10':
+            modelConfig['save_dir'] = modelConfig['save_dir'] + str(modelConfig['seed']) + '/'
+            modelConfig['sampled_dir'] = modelConfig['sampled_dir'] + str(modelConfig['seed']) + '/'
+            modelConfig['num_classes'] = 10
+        elif modelConfig['dataset'] == 'CIFAR100':
+            modelConfig['save_dir'] = modelConfig['save_dir'] + str(modelConfig['seed']) + 'CIFAR100/'
+            modelConfig['sampled_dir'] = modelConfig['sampled_dir'] + str(modelConfig['seed']) + 'CIFAR100/'
+            modelConfig['num_classes'] = 100
+
+        os.makedirs(modelConfig['save_dir'], exist_ok=True)
+        os.makedirs(modelConfig['sampled_dir'], exist_ok=True)
+
+        if modelConfig["state"] == "train":
+            train(modelConfig)
+        elif modelConfig["state"] == "eval":
+            eval(modelConfig)
+        elif modelConfig["state"] == "generate":
+            generate(modelConfig)
+        elif modelConfig["state"] == "traingen":
+            train(modelConfig)
+            generate(modelConfig)
 
 
 if __name__ == '__main__':
